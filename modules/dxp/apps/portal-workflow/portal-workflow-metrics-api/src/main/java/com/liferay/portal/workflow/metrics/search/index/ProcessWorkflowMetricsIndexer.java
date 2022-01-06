66088dd6d8da45154736a0f1b54186749b26b43c
Selton Guedes <sfgs@ecomp.poli.br>
2022-01-06 13:49:31 -0300

    LPS-138924 Add new deleteProcess method

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-api/src/main/java/com/liferay/portal/workflow/metrics/search/index/ProcessWorkflowMetricsIndexer.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-api/src/main/java/com/liferay/portal/workflow/metrics/search/index/ProcessWorkflowMetricsIndexer.java
@@ -16,6 +16,7 @@ package com.liferay.portal.workflow.metrics.search.index;
 
 import com.liferay.portal.search.document.Document;
 import com.liferay.portal.workflow.metrics.model.AddProcessRequest;
+import com.liferay.portal.workflow.metrics.model.DeleteProcessRequest;
 
 import java.util.Date;
 import java.util.Locale;
