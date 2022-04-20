c450fd27078b38ee115c7e33136dd65fa2e0c730
Selton Guedes <sfgs@ecomp.poli.br>
2022-04-20 13:29:56 -0300

    LPS-151716 Create interface for Object Validation Rule Element

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/index.d.ts
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/index.d.ts
@@ -59,6 +59,17 @@ interface ObjectValidation {
 	script: string;
 }
 
+interface ObjectValidationRuleElement {
+	items: ObjectValidationRuleElementItem[];
+	label: string;
+}
+
+interface ObjectValidationRuleElementItem {
+	content: string;
+	label: string;
+	tooltip: string;
+}
+
 interface ObjectValidationType {
 	label: string;
 }
