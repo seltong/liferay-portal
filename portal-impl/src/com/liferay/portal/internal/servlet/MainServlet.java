414fc7e50898fd3b75c0e04d15b78dc5c2a876dd
Selton Guedes <sfgs@ecomp.poli.br>
2022-06-14 09:50:58 -0300

    WIP

--- portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.internal.servlet;
 
-import com.liferay.object.exception.ObjectValidationRuleScriptException;
 import com.liferay.petra.io.StreamUtil;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
@@ -533,8 +532,8 @@ public class MainServlet extends HttpServlet {
 			if (_log.isDebugEnabled()) {
 				_log.debug("Authenticated user id " + userId);
 			}
-		} catch(ObjectValidationRuleScriptException objectValidationRuleScriptException) {
-			throw new ServletException(objectValidationRuleScriptException);
+		} catch(PortalException portalException) {
+			throw new ServletException(portalException);
 		}
 		catch (Exception exception) {
 			_log.error(exception);
