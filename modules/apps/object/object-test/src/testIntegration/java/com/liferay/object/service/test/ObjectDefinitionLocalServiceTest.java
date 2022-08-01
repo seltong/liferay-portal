9db7019e3087eca5c504efc5a4f60410ab3dc70a
Selton Guedes <sfgs@ecomp.poli.br>
2022-08-01 17:23:32 -0300

    LPS-159070 Add feature flag to the integration tests

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceTest.java
@@ -56,9 +56,11 @@ import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.TextFormatter;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.util.PropsUtil;
 
 import java.sql.Connection;
 
@@ -88,6 +90,10 @@ public class ObjectDefinitionLocalServiceTest {
 
 	@Test
 	public void testAddCustomObjectDefinition() throws Exception {
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-158821", "true"
+			).build());
 
 		// Label is null
 
@@ -406,6 +412,11 @@ public class ObjectDefinitionLocalServiceTest {
 			WorkflowConstants.STATUS_APPROVED, objectDefinition.getStatus());
 
 		_objectDefinitionLocalService.deleteObjectDefinition(objectDefinition);
+
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-158821", "false"
+			).build());
 	}
 
 	@Test
@@ -589,6 +600,10 @@ public class ObjectDefinitionLocalServiceTest {
 
 	@Test
 	public void testAddSystemObjectDefinition() throws Exception {
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-158821", "true"
+			).build());
 
 		// Label is null
 
@@ -889,6 +904,11 @@ public class ObjectDefinitionLocalServiceTest {
 		}
 
 		_objectDefinitionLocalService.deleteObjectDefinition(objectDefinition);
+
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-158821", "false"
+			).build());
 	}
 
 	@Test
