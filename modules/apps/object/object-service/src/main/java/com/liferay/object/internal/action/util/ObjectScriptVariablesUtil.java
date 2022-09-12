00f6389e9a5c66ded7601788c92af49482a956f6
Selton Guedes <sfgs@ecomp.poli.br>
2022-09-12 14:12:30 -0300

    LPS-151540 Create util class to provide variables to object scripts

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/util/ObjectScriptVariablesUtil.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.internal.action.util;
+
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectFieldLocalServiceUtil;
+import com.liferay.object.system.SystemObjectDefinitionMetadata;
+import com.liferay.object.system.SystemObjectDefinitionMetadataTracker;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.vulcan.dto.converter.DTOConverter;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Selton Guedes
+ */
+public class ObjectScriptVariablesUtil {
+
+	public static Map<String, Object> toVariables(
+		DTOConverterRegistry dtoConverterRegistry,
+		ObjectDefinition objectDefinition, JSONObject payloadJSONObject,
+		SystemObjectDefinitionMetadataTracker
+			systemObjectDefinitionMetadataTracker) {
+
+		Map<String, Object> allowedVariables =
+			HashMapBuilder.<String, Object>put(
+				"creator", payloadJSONObject.get("userId")
+			).build();
+
+		Map<String, Object> allVariables = null;
+
+		if (objectDefinition.isSystem()) {
+			String contentType = _getContentType(
+				dtoConverterRegistry, objectDefinition,
+				systemObjectDefinitionMetadataTracker);
+
+			allVariables.putAll(
+				(Map<String, Object>)payloadJSONObject.get(
+					"model" + objectDefinition.getName()));
+			allVariables.putAll(
+				(Map<String, Object>)payloadJSONObject.get(
+					"modelDTO" + contentType));
+		}
+		else {
+			allVariables.putAll(
+				(Map<String, Object>)payloadJSONObject.get("objectEntry"));
+
+			allVariables.putAll(
+				(Map<String, Object>)allVariables.get("values"));
+
+			allVariables.remove("values");
+
+			Object objectEntryId = allVariables.get("objectEntryId");
+
+			if (objectEntryId != null) {
+				allowedVariables.put("id", objectEntryId);
+			}
+		}
+
+		allVariables.remove("creator");
+
+		List<ObjectField> objectFields =
+			ObjectFieldLocalServiceUtil.getObjectFields(
+				objectDefinition.getObjectDefinitionId());
+
+		for (ObjectField objectField : objectFields) {
+			if (!allowedVariables.containsKey(objectField.getName())) {
+				allowedVariables.put(
+					objectField.getName(),
+					allVariables.get(objectField.getName()));
+			}
+		}
+
+		return allowedVariables;
+	}
+
+	private static String _getContentType(
+		DTOConverterRegistry dtoConverterRegistry,
+		ObjectDefinition objectDefinition,
+		SystemObjectDefinitionMetadataTracker
+			systemObjectDefinitionMetadataTracker) {
+
+		DTOConverter<?, ?> dtoConverter = dtoConverterRegistry.getDTOConverter(
+			objectDefinition.getClassName());
+
+		if (dtoConverter == null) {
+			SystemObjectDefinitionMetadata systemObjectDefinitionMetadata =
+				systemObjectDefinitionMetadataTracker.
+					getSystemObjectDefinitionMetadata(
+						objectDefinition.getName());
+
+			return systemObjectDefinitionMetadata.getModelClassName();
+		}
+
+		return dtoConverter.getContentType();
+	}
+
+}
