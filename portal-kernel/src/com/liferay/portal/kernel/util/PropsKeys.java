cf05cf39554d2a892f0ac5ea40f7ecb11bf18baa
Selton Guedes <sfgs@ecomp.poli.br>
2022-09-29 12:25:33 -0300

    LPS-151540 Create new property to object scripts variables version

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1840,6 +1840,9 @@ public interface PropsKeys {
 	public static final String NOTIFICATIONS_MAX_EVENTS =
 		"notifications.max.events";
 
+	public static final String OBJECT_DEFINITION_SCRIPT_SYNTAX_VERSION =
+		"object.definition.script.syntax.version";
+
 	public static final String OBJECT_NESTED_FIELDS_MAX_QUERY_DEPTH =
 		"object.nested.fields.max.query.depth";
 
