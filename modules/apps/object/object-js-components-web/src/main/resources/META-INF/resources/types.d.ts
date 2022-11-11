2c73cfd5761003eed3faedc6183f2a58f6879e87
Selton Guedes <sfgs@ecomp.poli.br>
2022-11-11 16:36:30 -0300

    LPS-168318 Add externalReferenceCode property in ObjectDefinition interface

--- modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/types.d.ts
+++ modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/types.d.ts
@@ -18,6 +18,7 @@ interface ObjectDefinition {
 	active: boolean;
 	dateCreated: string;
 	dateModified: string;
+	externalReferenceCode: string;
 	id: number;
 	label: LocalizedValue<string>;
 	name: string;
