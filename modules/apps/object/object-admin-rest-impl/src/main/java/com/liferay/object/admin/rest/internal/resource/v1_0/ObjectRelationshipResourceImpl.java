06684afb2053a84b818b133ed4eb59f944a90a94
Selton Guedes <sfgs@ecomp.poli.br>
2022-11-11 09:25:27 -0300

    LPS-168318 Get objectDefinitionId2 by objectDefinitionExternalReferenceCode2

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectRelationshipResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectRelationshipResourceImpl.java
@@ -128,30 +128,22 @@ public class ObjectRelationshipResourceImpl
 			Long objectDefinitionId, ObjectRelationship objectRelationship)
 		throws Exception {
 
-		long objectDefinitionId2 = GetterUtil.getLong(
-			objectRelationship.getObjectDefinitionId2());
+		com.liferay.object.model.ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					contextCompany.getCompanyId(),
+					objectRelationship.
+						getObjectDefinitionExternalReferenceCode2());
 
-		if ((objectDefinitionId2 == 0) &&
-			(objectRelationship.getObjectDefinitionExternalReferenceCode2() !=
-				null)) {
-
-			com.liferay.object.model.ObjectDefinition objectDefinition =
-				_objectDefinitionLocalService.
-					fetchObjectDefinitionByExternalReferenceCode(
-						contextCompany.getCompanyId(),
-						objectRelationship.
-							getObjectDefinitionExternalReferenceCode2());
-
-			if (objectDefinition == null) {
-				objectDefinition =
-					_objectDefinitionLocalService.addObjectDefinition(
-						objectRelationship.
-							getObjectDefinitionExternalReferenceCode2(),
-						contextUser.getUserId());
-			}
-
-			objectDefinitionId2 = objectDefinition.getObjectDefinitionId();
+		if (objectDefinition == null) {
+			objectDefinition =
+				_objectDefinitionLocalService.addObjectDefinition(
+					objectRelationship.
+						getObjectDefinitionExternalReferenceCode2(),
+					contextUser.getUserId());
 		}
+
+		long objectDefinitionId2 = objectDefinition.getObjectDefinitionId();
 
 		return _toObjectRelationship(
 			_objectRelationshipService.addObjectRelationship(
