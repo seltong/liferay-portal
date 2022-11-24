d577ec81b20f422286831404dc8916c93e4940f1
Selton Guedes <sfgs@ecomp.poli.br>
2022-11-24 11:21:58 -0300

    LPS-169559 Adjust tests to use and check external reference code

--- modules/apps/list-type/list-type-test/src/testIntegration/java/com/liferay/list/type/service/test/ListTypeEntryServiceTest.java
+++ modules/apps/list-type/list-type-test/src/testIntegration/java/com/liferay/list/type/service/test/ListTypeEntryServiceTest.java
@@ -154,7 +154,8 @@ public class ListTypeEntryServiceTest {
 
 	private ListTypeEntry _addListTypeEntry(User user) throws Exception {
 		return _listTypeEntryLocalService.addListTypeEntry(
-			user.getUserId(), _listTypeDefinition.getListTypeDefinitionId(),
+			RandomTestUtil.randomString(), user.getUserId(),
+			_listTypeDefinition.getListTypeDefinitionId(),
 			RandomTestUtil.randomString(),
 			Collections.singletonMap(
 				LocaleUtil.US, RandomTestUtil.randomString()));
@@ -231,10 +232,16 @@ public class ListTypeEntryServiceTest {
 
 			listTypeEntry = _addListTypeEntry(user);
 
+			String externalReferenceCode = RandomTestUtil.randomString();
+
 			listTypeEntry = _listTypeEntryService.updateListTypeEntry(
-				listTypeEntry.getListTypeEntryId(),
+				externalReferenceCode, listTypeEntry.getListTypeEntryId(),
 				Collections.singletonMap(
 					LocaleUtil.US, RandomTestUtil.randomString()));
+
+			Assert.assertEquals(
+				externalReferenceCode,
+				listTypeEntry.getExternalReferenceCode());
 		}
 		finally {
 			if (listTypeEntry != null) {
--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectFieldLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectFieldLocalServiceTest.java
@@ -117,7 +117,7 @@ public class ObjectFieldLocalServiceTest {
 		_listTypeEntryKey = RandomTestUtil.randomString();
 
 		_listTypeEntryLocalService.addListTypeEntry(
-			TestPropsValues.getUserId(),
+			RandomTestUtil.randomString(), TestPropsValues.getUserId(),
 			_listTypeDefinition.getListTypeDefinitionId(), _listTypeEntryKey,
 			LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()));
 	}
