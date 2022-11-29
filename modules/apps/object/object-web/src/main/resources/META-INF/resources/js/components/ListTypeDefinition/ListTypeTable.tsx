2da93c1b0d46b6eb8e06da248551b6f0165836e1
Selton Guedes <sfgs@ecomp.poli.br>
2022-11-29 11:54:17 -0300

    LPS-169636 Show external reference code input on ListTypeEntry edit modal

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ListTypeDefinition/ListTypeTable.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ListTypeDefinition/ListTypeTable.tsx
@@ -29,6 +29,7 @@ interface IProps {
 }
 
 interface ItemData {
+	externalReferenceCode: string;
 	id: number;
 	key: string;
 	name: {props: {id: number}};
@@ -87,6 +88,7 @@ function getDataSetProps(
 		if (action.id === 'addListTypeEntry') {
 			fireModal({
 				header: Liferay.Language.get('edit-item'),
+				itemExternalReferenceCode: itemData.externalReferenceCode,
 				itemId: itemData.id,
 				itemKey: itemData.key,
 				modalType: 'edit',
