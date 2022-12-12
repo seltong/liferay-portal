5c62bcbd054fd64589e13e8cb073a80b79549c8c
Selton Guedes <sfgs@ecomp.poli.br>
2022-12-12 15:53:02 -0300

    WIP

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionLocalServiceImpl.java
@@ -20,27 +20,34 @@ import com.liferay.object.action.executor.ObjectActionExecutorRegistry;
 import com.liferay.object.constants.ObjectActionConstants;
 import com.liferay.object.constants.ObjectActionExecutorConstants;
 import com.liferay.object.constants.ObjectActionTriggerConstants;
+import com.liferay.object.constants.ObjectDefinitionConstants;
 import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.exception.NoSuchObjectFieldException;
 import com.liferay.object.exception.ObjectActionConditionExpressionException;
 import com.liferay.object.exception.ObjectActionErrorMessageException;
 import com.liferay.object.exception.ObjectActionLabelException;
 import com.liferay.object.exception.ObjectActionNameException;
 import com.liferay.object.exception.ObjectActionParametersException;
 import com.liferay.object.exception.ObjectActionTriggerKeyException;
+import com.liferay.object.exception.ObjectFieldRelationshipTypeException;
 import com.liferay.object.internal.action.trigger.util.ObjectActionTriggerUtil;
 import com.liferay.object.model.ObjectAction;
 import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntryTable;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.scripting.exception.ObjectScriptingException;
 import com.liferay.object.scripting.validator.ObjectScriptingValidator;
 import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.service.base.ObjectActionLocalServiceBaseImpl;
 import com.liferay.object.service.persistence.ObjectDefinitionPersistence;
+import com.liferay.object.service.persistence.ObjectFieldPersistence;
+import com.liferay.object.util.LocalizedMapUtil;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.MessageBus;
@@ -48,6 +55,9 @@ import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.service.ResourceLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -59,6 +69,7 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.HashMap;
 import java.util.List;
@@ -246,6 +257,93 @@ public class ObjectActionLocalServiceImpl
 		return objectActionPersistence.update(objectAction);
 	}
 
+	private void _addSystemObjectFields(
+			String dbTableName, ObjectDefinition objectDefinition,
+			String pkObjectFieldName, boolean system, long userId)
+		throws PortalException {
+
+		_objectFieldLocalService.addSystemObjectField(
+			userId, objectDefinition.getObjectDefinitionId(),
+			ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+			ObjectEntryTable.INSTANCE.userName.getName(), dbTableName,
+			ObjectFieldConstants.DB_TYPE_STRING, null, false, false, null,
+			LocalizedMapUtil.getLocalizedMap(
+				_language.get(LocaleUtil.getDefault(), "author")),
+			"creator", false, false);
+
+		_objectFieldLocalService.addSystemObjectField(
+			userId, objectDefinition.getObjectDefinitionId(),
+			ObjectFieldConstants.BUSINESS_TYPE_DATE,
+			ObjectEntryTable.INSTANCE.createDate.getName(), dbTableName,
+			ObjectFieldConstants.DB_TYPE_DATE, null, false, false, null,
+			LocalizedMapUtil.getLocalizedMap(
+				_language.get(LocaleUtil.getDefault(), "create-date")),
+			"createDate", false, false);
+
+		_objectFieldLocalService.addSystemObjectField(
+			userId, objectDefinition.getObjectDefinitionId(),
+			ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+			ObjectEntryTable.INSTANCE.externalReferenceCode.getName(),
+			dbTableName, ObjectFieldConstants.DB_TYPE_STRING, null, false,
+			false, null,
+			LocalizedMapUtil.getLocalizedMap(
+				_language.get(
+					LocaleUtil.getDefault(), "external-reference-code")),
+			"externalReferenceCode", false, false);
+
+		String dbColumnName = ObjectEntryTable.INSTANCE.objectEntryId.getName();
+
+		if (system) {
+			dbColumnName = pkObjectFieldName;
+		}
+
+		_objectFieldLocalService.addSystemObjectField(
+			userId, objectDefinition.getObjectDefinitionId(),
+			ObjectFieldConstants.BUSINESS_TYPE_LONG_INTEGER, dbColumnName,
+			dbTableName, ObjectFieldConstants.DB_TYPE_LONG, null, true, true,
+			null,
+			LocalizedMapUtil.getLocalizedMap(
+				_language.get(LocaleUtil.getDefault(), "id")),
+			"id", false, false);
+
+		_objectFieldLocalService.addSystemObjectField(
+			userId, objectDefinition.getObjectDefinitionId(),
+			ObjectFieldConstants.BUSINESS_TYPE_DATE,
+			ObjectEntryTable.INSTANCE.modifiedDate.getName(), dbTableName,
+			ObjectFieldConstants.DB_TYPE_DATE, null, false, false, null,
+			LocalizedMapUtil.getLocalizedMap(
+				_language.get(LocaleUtil.getDefault(), "modified-date")),
+			"modifiedDate", false, false);
+
+		_objectFieldLocalService.addSystemObjectField(
+			userId, objectDefinition.getObjectDefinitionId(),
+			ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+			ObjectEntryTable.INSTANCE.status.getName(), dbTableName,
+			ObjectFieldConstants.DB_TYPE_INTEGER, null, false, false, null,
+			LocalizedMapUtil.getLocalizedMap(
+				_language.get(LocaleUtil.getDefault(), "status")),
+			"status", false, false);
+	}
+
+	private ObjectDefinition _updateTitleObjectFieldId(
+			ObjectDefinition objectDefinition, String titleObjectFieldName)
+		throws PortalException {
+
+		if (Validator.isNull(titleObjectFieldName)) {
+			titleObjectFieldName = "id";
+		}
+
+		ObjectField objectField = _objectFieldPersistence.findByODI_N(
+			objectDefinition.getObjectDefinitionId(), titleObjectFieldName);
+
+		_validateObjectFieldId(
+			objectDefinition, objectField.getObjectFieldId());
+
+		objectDefinition.setTitleObjectFieldId(objectField.getObjectFieldId());
+
+		return _objectDefinitionPersistence.update(objectDefinition);
+	}
+
 	private void _validateErrorMessage(
 			Map<Locale, String> errorMessageMap, String objectActionTriggerKey)
 		throws PortalException {
@@ -358,6 +456,31 @@ public class ObjectActionLocalServiceImpl
 		}
 	}
 
+	private void _validateObjectFieldId(
+			ObjectDefinition objectDefinition, long objectFieldId)
+		throws PortalException {
+
+		if (objectFieldId <= 0) {
+			return;
+		}
+
+		ObjectField objectField = _objectFieldLocalService.fetchObjectField(
+			objectFieldId);
+
+		if ((objectField == null) ||
+			(objectField.getObjectDefinitionId() !=
+				objectDefinition.getObjectDefinitionId())) {
+
+			throw new NoSuchObjectFieldException();
+		}
+
+		if (Validator.isNotNull(objectField.getRelationshipType())) {
+			throw new ObjectFieldRelationshipTypeException(
+				"Description and title object fields cannot have a " +
+					"relationship type");
+		}
+	}
+
 	private void _validateParametersUnicodeProperties(
 			String conditionExpression, String objectActionExecutorKey,
 			String objectActionTriggerKey,
@@ -404,22 +527,79 @@ public class ObjectActionLocalServiceImpl
 				objectActionExecutorKey,
 				ObjectActionExecutorConstants.KEY_UPDATE_OBJECT_ENTRY)) {
 
-			long objectDefinitionId = GetterUtil.getLong(
-				parametersUnicodeProperties.get("objectDefinitionId"));
+			ObjectDefinition objectDefinition = null;
 
-			ObjectDefinition objectDefinition =
-				_objectDefinitionPersistence.fetchByPrimaryKey(
-					objectDefinitionId);
+			String externalReferenceCode = GetterUtil.getString(
+				parametersUnicodeProperties.get(
+					"objectDefinitionExternalReferenceCode"));
+
+			if (Validator.isNotNull(externalReferenceCode)) {
+				objectDefinition = _objectDefinitionPersistence.fetchByERC_C(
+					externalReferenceCode, CompanyThreadLocal.getCompanyId());
+
+				if (objectDefinition == null) {
+					objectDefinition = _objectDefinitionPersistence.create(
+						counterLocalService.increment());
+
+					objectDefinition.setExternalReferenceCode(
+						externalReferenceCode);
+
+					User user = _userLocalService.getUser(
+						PrincipalThreadLocal.getUserId());
+
+					objectDefinition.setCompanyId(user.getCompanyId());
+					objectDefinition.setUserId(user.getUserId());
+					objectDefinition.setUserName(user.getFullName());
+
+					objectDefinition.setActive(true);
+					objectDefinition.setLabel(externalReferenceCode);
+					objectDefinition.setName(externalReferenceCode);
+					objectDefinition.setPluralLabel(externalReferenceCode);
+					objectDefinition.setStorageType(
+						ObjectDefinitionConstants.STORAGE_TYPE_DEFAULT);
+					objectDefinition.setSystem(false);
+					objectDefinition.setStatus(WorkflowConstants.STATUS_DRAFT);
+
+					objectDefinition = _objectDefinitionPersistence.update(
+						objectDefinition);
+
+					_resourceLocalService.addResources(
+						objectDefinition.getCompanyId(), 0,
+						objectDefinition.getUserId(),
+						ObjectDefinition.class.getName(),
+						objectDefinition.getObjectDefinitionId(), false, true,
+						true);
+
+					_addSystemObjectFields(
+						ObjectEntryTable.INSTANCE.getTableName(),
+						objectDefinition,
+						ObjectEntryTable.INSTANCE.objectEntryId.getName(),
+						objectDefinition.isSystem(), user.getUserId());
+
+					objectDefinition = _updateTitleObjectFieldId(
+						objectDefinition, null);
+				}
+			}
+			else {
+				long objectDefinitionId = GetterUtil.getLong(
+					parametersUnicodeProperties.get("objectDefinitionId"));
+
+				objectDefinition =
+					_objectDefinitionPersistence.fetchByPrimaryKey(
+						objectDefinitionId);
+			}
 
 			if ((objectDefinition == null) || !objectDefinition.isActive() ||
 				!objectDefinition.isApproved() || objectDefinition.isSystem()) {
 
+				errorMessageKeys.put(
+					"objectDefinitionExternalReferenceCode", "invalid");
 				errorMessageKeys.put("objectDefinitionId", "invalid");
 			}
 			else {
 				_validatePredefinedValues(
 					errorMessageKeys, objectActionExecutorKey,
-					objectDefinitionId,
+					objectDefinition.getObjectDefinitionId(),
 					_jsonFactory.createJSONArray(
 						parametersUnicodeProperties.get("predefinedValues")));
 			}
@@ -551,6 +731,9 @@ public class ObjectActionLocalServiceImpl
 	private JSONFactory _jsonFactory;
 
 	@Reference
+	private Language _language;
+
+	@Reference
 	private MessageBus _messageBus;
 
 	@Reference
@@ -563,7 +746,13 @@ public class ObjectActionLocalServiceImpl
 	private ObjectFieldLocalService _objectFieldLocalService;
 
 	@Reference
+	private ObjectFieldPersistence _objectFieldPersistence;
+
+	@Reference
 	private ObjectScriptingValidator _objectScriptingValidator;
+
+	@Reference
+	private ResourceLocalService _resourceLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
