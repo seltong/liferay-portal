330301e4e1c8c5a35363051c06b253fa2ce2e10e
Selton Guedes <sfgs@ecomp.poli.br>
2023-03-15 15:07:15 -0300

    LPS-178220 Allow use actions and validations in modifiable system objects

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/NotificationTemplateObjectActionExecutorImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/NotificationTemplateObjectActionExecutorImpl.java
@@ -83,8 +83,8 @@ public class NotificationTemplateObjectActionExecutorImpl
 			).userId(
 				userId
 			).portletId(
-				objectDefinition.isSystem() ? StringPool.BLANK :
-					objectDefinition.getPortletId()
+				objectDefinition.isUnmodifiableSystemObject() ?
+					StringPool.BLANK : objectDefinition.getPortletId()
 			).build());
 	}
 
