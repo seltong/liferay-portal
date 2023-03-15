5aca8ba11309807f5de26b934080eaefcf953970
Selton Guedes <sfgs@ecomp.poli.br>
2023-03-15 15:07:15 -0300

    LPS-178220 Allow use actions and validations in modifiable system objects

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/util/ObjectEntryVariablesUtil.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/util/ObjectEntryVariablesUtil.java
@@ -316,7 +316,7 @@ public class ObjectEntryVariablesUtil {
 		SystemObjectDefinitionMetadataRegistry
 			systemObjectDefinitionMetadataRegistry) {
 
-		if (!objectDefinition.isSystem()) {
+		if (!objectDefinition.isUnmodifiableSystemObject()) {
 			return "ObjectEntry";
 		}
 
@@ -345,7 +345,7 @@ public class ObjectEntryVariablesUtil {
 
 		Map<String, Object> variables = new HashMap<>();
 
-		if (objectDefinition.isSystem()) {
+		if (objectDefinition.isUnmodifiableSystemObject()) {
 			SystemObjectDefinitionMetadata systemObjectDefinitionMetadata =
 				systemObjectDefinitionMetadataRegistry.
 					getSystemObjectDefinitionMetadata(
