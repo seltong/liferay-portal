f2e15d844427cb7606bf60df272974932be1c71e
Selton Guedes <sfgs@ecomp.poli.br>
2023-04-05 11:18:41 -0300

    LPS-180861 Add integration tests to ensure that is possible create object relationship for modifiable system objects

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectRelationshipLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectRelationshipLocalServiceTest.java
@@ -46,8 +46,10 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 import java.sql.Connection;
@@ -56,6 +58,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
@@ -94,6 +97,11 @@ public class ObjectRelationshipLocalServiceTest {
 
 	@Before
 	public void setUp() throws Exception {
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-167253", "true"
+			).build());
+
 		_objectDefinition1 = ObjectDefinitionTestUtil.addObjectDefinition(
 			_objectDefinitionLocalService,
 			Arrays.asList(
@@ -120,8 +128,38 @@ public class ObjectRelationshipLocalServiceTest {
 				TestPropsValues.getUserId(),
 				_objectDefinition2.getObjectDefinitionId());
 
+		_modifiableSystemObjectDefinition =
+			ObjectDefinitionTestUtil.addModifiableSystemObjectDefinition(
+				TestPropsValues.getUserId(), null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				"Test", null, null,
+				LocalizedMapUtil.getLocalizedMap(RandomTestUtil.randomString()),
+				ObjectDefinitionConstants.SCOPE_COMPANY, null, 1,
+				_objectDefinitionLocalService,
+				Arrays.asList(
+					ObjectFieldUtil.createObjectField(
+						ObjectFieldConstants.BUSINESS_TYPE_TEXT,
+						ObjectFieldConstants.DB_TYPE_STRING, true, true, null,
+						"First Name", "firstName", true)));
+
+		_modifiableSystemObjectDefinition =
+			_objectDefinitionLocalService.publishCustomObjectDefinition(
+				TestPropsValues.getUserId(),
+				_modifiableSystemObjectDefinition.getObjectDefinitionId());
+
 		_systemObjectDefinition2 = _addSystemObjectDefinition(
 			"/o/test-endpoint/entries");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_objectDefinitionLocalService.deleteObjectDefinition(
+			_modifiableSystemObjectDefinition.getObjectDefinitionId());
+
+		PropsUtil.addProperties(
+			UnicodePropertiesBuilder.setProperty(
+				"feature.flag.LPS-167253", "false"
+			).build());
 	}
 
 	@Test
@@ -244,18 +282,50 @@ public class ObjectRelationshipLocalServiceTest {
 			ObjectRelationshipConstants.DELETION_TYPE_CASCADE,
 			_objectDefinition1, _systemObjectDefinition2);
 		_testAddObjectRelationshipManyToMany(
+			ObjectRelationshipConstants.DELETION_TYPE_CASCADE,
+			_modifiableSystemObjectDefinition, _systemObjectDefinition2);
+		_testAddObjectRelationshipManyToMany(
+			ObjectRelationshipConstants.DELETION_TYPE_CASCADE,
+			_objectDefinition1, _modifiableSystemObjectDefinition);
+		_testAddObjectRelationshipManyToMany(
 			ObjectRelationshipConstants.DELETION_TYPE_DISASSOCIATE,
 			_objectDefinition1, _systemObjectDefinition2);
 		_testAddObjectRelationshipManyToMany(
+			ObjectRelationshipConstants.DELETION_TYPE_DISASSOCIATE,
+			_objectDefinition1, _modifiableSystemObjectDefinition);
+		_testAddObjectRelationshipManyToMany(
+			ObjectRelationshipConstants.DELETION_TYPE_DISASSOCIATE,
+			_modifiableSystemObjectDefinition, _systemObjectDefinition2);
+		_testAddObjectRelationshipManyToMany(
 			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
 			_objectDefinition1, _systemObjectDefinition2);
 		_testAddObjectRelationshipManyToMany(
 			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
+			_modifiableSystemObjectDefinition, _systemObjectDefinition2);
+		_testAddObjectRelationshipManyToMany(
+			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
+			_objectDefinition1, _modifiableSystemObjectDefinition);
+		_testAddObjectRelationshipManyToMany(
+			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
 			_systemObjectDefinition2, _objectDefinition1);
+		_testAddObjectRelationshipManyToMany(
+			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
+			_modifiableSystemObjectDefinition, _objectDefinition1);
+		_testAddObjectRelationshipManyToMany(
+			ObjectRelationshipConstants.DELETION_TYPE_PREVENT,
+			_systemObjectDefinition2, _modifiableSystemObjectDefinition);
 		_testAddObjectRelationshipOneToMany(
 			_objectDefinition1, _systemObjectDefinition2);
 		_testAddObjectRelationshipOneToMany(
+			_modifiableSystemObjectDefinition, _systemObjectDefinition2);
+		_testAddObjectRelationshipOneToMany(
+			_objectDefinition1, _modifiableSystemObjectDefinition);
+		_testAddObjectRelationshipOneToMany(
 			_systemObjectDefinition2, _objectDefinition1);
+		_testAddObjectRelationshipOneToMany(
+			_modifiableSystemObjectDefinition, _objectDefinition1);
+		_testAddObjectRelationshipOneToMany(
+			_systemObjectDefinition2, _modifiableSystemObjectDefinition);
 
 		_testCreateManyToManyObjectRelationshipTable(_systemObjectDefinition2);
 
@@ -781,6 +851,9 @@ public class ObjectRelationshipLocalServiceTest {
 	private static ObjectDefinitionLocalService _objectDefinitionLocalService;
 
 	private static ObjectDefinition _systemObjectDefinition1;
+
+	@DeleteAfterTestRun
+	private ObjectDefinition _modifiableSystemObjectDefinition;
 
 	@DeleteAfterTestRun
 	private ObjectDefinition _objectDefinition1;
