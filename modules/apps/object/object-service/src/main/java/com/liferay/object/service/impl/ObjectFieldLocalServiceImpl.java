051ca53e49cc704397a6f25e8b9d6c753ba98f95
Selton Guedes <selton.guedes@liferay.com>
2023-05-25 11:25:58 -0300

    LPS-184537 Localizable object fields can not be required

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectFieldLocalServiceImpl.java
@@ -633,7 +633,8 @@ public class ObjectFieldLocalServiceImpl
 			businessType, dbType, indexed, indexedAsKeyword, indexedLanguageId);
 		_validateLabel(labelMap, newObjectField);
 		_validateLocalized(
-			businessType, localized, oldObjectField.getObjectDefinition());
+			businessType, localized, oldObjectField.getObjectDefinition(),
+			required);
 
 		ObjectDefinition objectDefinition =
 			_objectDefinitionPersistence.findByPrimaryKey(
@@ -774,7 +775,7 @@ public class ObjectFieldLocalServiceImpl
 		_validateIndexed(
 			businessType, dbType, indexed, indexedAsKeyword, indexedLanguageId);
 		_validateLabel(labelMap, null);
-		_validateLocalized(businessType, localized, objectDefinition);
+		_validateLocalized(businessType, localized, objectDefinition, required);
 		_validateName(0, objectDefinition, name, system);
 		_validateReadOnlyAndReadOnlyConditionExpression(
 			businessType, readOnly, readOnlyConditionExpression);
@@ -1251,11 +1252,16 @@ public class ObjectFieldLocalServiceImpl
 
 	private void _validateLocalized(
 			String businessType, boolean localized,
-			ObjectDefinition objectDefinition)
+			ObjectDefinition objectDefinition, boolean required)
 		throws PortalException {
 
 		if (!localized) {
 			return;
+		}
+
+		if (required) {
+			throw new ObjectFieldLocalizedException(
+				"Localized object fields must not be required");
 		}
 
 		if (!businessType.equals(
