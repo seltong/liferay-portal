d31e3ad39835f404dd46903dca8c8b498cffb0af
Selton Guedes <selton.guedes@liferay.com>
2023-05-30 13:39:07 -0300

    LPS-172418 Add ObjectRelationship ERC upgrade process

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -305,6 +305,19 @@ public class ObjectServiceUpgradeStepRegistrator
 			"5.3.0", "5.3.1",
 			new com.liferay.object.internal.upgrade.v5_3_1.
 				SchemaUpgradeProcess());
+
+		registry.register(
+			"5.3.1", "5.4.0",
+			new BaseExternalReferenceCodeUpgradeProcess() {
+
+				@Override
+				protected String[][] getTableAndPrimaryKeyColumnNames() {
+					return new String[][] {
+						{"ObjectRelationship", "objectRelationshipId"}
+					};
+				}
+
+			});
 	}
 
 	@Reference
