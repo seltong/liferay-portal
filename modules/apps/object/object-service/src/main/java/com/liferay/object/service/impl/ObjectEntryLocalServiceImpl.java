0fb6b8abce1e94e59fdfabc09ce5eb8694a6910e
Selton Guedes <selton.guedes@liferay.com>
2023-05-31 09:42:08 -0300

    LPS-184535 Rename and Sort

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -173,7 +173,6 @@ import com.liferay.portal.search.searcher.Searcher;
 import com.liferay.portal.search.sort.SortOrder;
 import com.liferay.portal.search.sort.Sorts;
 import com.liferay.portal.util.PropsValues;
-import com.liferay.portal.vulcan.util.ObjectMapperUtil;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -947,16 +946,12 @@ public class ObjectEntryLocalServiceImpl
 			objectEntry.getObjectDefinitionId(), rows.get(0),
 			selectExpressions);
 
+		_addLocalizedObjectFieldValues(
+			dynamicObjectDefinitionLocalizationTable,
+			objectEntry.getObjectEntryId(), values);
+
 		_addObjectRelationshipERCFieldValue(
 			objectEntry.getObjectDefinitionId(), values);
-
-		if (FeatureFlagManagerUtil.isEnabled("LPS-172017") &&
-			(dynamicObjectDefinitionLocalizationTable != null)) {
-
-			_addLocalizedObjectFieldValues(
-				dynamicObjectDefinitionLocalizationTable,
-				objectEntry.getObjectEntryId(), values);
-		}
 
 		return values;
 	}
@@ -1426,26 +1421,29 @@ public class ObjectEntryLocalServiceImpl
 			dynamicObjectDefinitionLocalizationTable,
 		long objectEntryId, Map<String, Serializable> values) {
 
-		Column<DynamicObjectDefinitionLocalizationTable, Long>
-			foreignKeyColumn =
-				dynamicObjectDefinitionLocalizationTable.getForeignKeyColumn();
+		if (!FeatureFlagManagerUtil.isEnabled("LPS-172017") ||
+			(dynamicObjectDefinitionLocalizationTable == null)) {
 
-		List<Object[]> localizedValues = ObjectMapperUtil.readValue(
-			List.class,
-			(Serializable)objectEntryPersistence.dslQuery(
-				DSLQueryFactoryUtil.select(
-					ArrayUtil.append(
-						_getSelectExpressions(
-							dynamicObjectDefinitionLocalizationTable),
-						dynamicObjectDefinitionLocalizationTable.
-							getLanguageIdColumn())
-				).from(
-					dynamicObjectDefinitionLocalizationTable
-				).where(
-					foreignKeyColumn.eq(objectEntryId)
-				)));
+			return;
+		}
 
-		if (ListUtil.isEmpty(localizedValues)) {
+		List<Object[]> rows = objectEntryPersistence.dslQuery(
+			DSLQueryFactoryUtil.select(
+				ArrayUtil.append(
+					_getSelectExpressions(
+						dynamicObjectDefinitionLocalizationTable),
+					dynamicObjectDefinitionLocalizationTable.
+						getLanguageIdColumn())
+			).from(
+				dynamicObjectDefinitionLocalizationTable
+			).where(
+				dynamicObjectDefinitionLocalizationTable.getForeignKeyColumn(
+				).eq(
+					objectEntryId
+				)
+			));
+
+		if (ListUtil.isEmpty(rows)) {
 			return;
 		}
 
@@ -1454,19 +1452,21 @@ public class ObjectEntryLocalServiceImpl
 				dynamicObjectDefinitionLocalizationTable.
 					getObjectFieldColumns();
 
+		int languageIdColumnPosition = objectFieldColumns.size();
+
 		for (int i = 0; i < objectFieldColumns.size(); i++) {
-			Map<String, String> localizedObjectFieldValue = new HashMap<>();
+			Map<String, String> localizedValues = new HashMap<>();
 
-			for (Object[] localizedValue : localizedValues) {
-				Object value = localizedValue[i];
+			for (Object[] row : rows) {
+				Object localizedValue = row[i];
 
-				if (Validator.isNull(value)) {
+				if (Validator.isNull(localizedValue)) {
 					continue;
 				}
 
-				localizedObjectFieldValue.put(
-					String.valueOf(localizedValue[objectFieldColumns.size()]),
-					String.valueOf(value));
+				localizedValues.put(
+					String.valueOf(row[languageIdColumnPosition]),
+					String.valueOf(localizedValue));
 			}
 
 			Column<DynamicObjectDefinitionLocalizationTable, ?>
@@ -1474,7 +1474,7 @@ public class ObjectEntryLocalServiceImpl
 
 			values.put(
 				objectFieldColumn.getName() + "i18n",
-				(Serializable)localizedObjectFieldValue);
+				(Serializable)localizedValues);
 		}
 	}
 
