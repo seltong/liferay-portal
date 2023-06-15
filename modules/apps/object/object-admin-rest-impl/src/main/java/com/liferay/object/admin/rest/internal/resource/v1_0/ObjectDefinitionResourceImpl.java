e87b5f7b525bd2cef696cc1930866f1aa2f19d96
Selton Guedes <selton.guedes@liferay.com>
2023-06-15 15:54:05 -0300

    LPS-172418 ObjectRelationship must be created before ObjectLayout because the ObjectRelationship externalReferenceCode is used in the ObjectLayoutTab

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -802,20 +802,6 @@ public class ObjectDefinitionResourceImpl
 			}
 		}
 
-		if (objectLayouts != null) {
-			ObjectLayoutResource.Builder builder =
-				_objectLayoutResourceFactory.create();
-
-			ObjectLayoutResource objectLayoutResource = builder.user(
-				contextUser
-			).build();
-
-			for (ObjectLayout objectLayout : objectLayouts) {
-				objectLayoutResource.postObjectDefinitionObjectLayout(
-					objectDefinitionId, objectLayout);
-			}
-		}
-
 		if (objectRelationships != null) {
 			ObjectRelationshipResource.Builder builder =
 				_objectRelationshipResourceFactory.create();
@@ -854,6 +840,20 @@ public class ObjectDefinitionResourceImpl
 						_objectRelationshipLocalService.getObjectRelationship(
 							objectRelationship.getId()));
 				}
+			}
+		}
+
+		if (objectLayouts != null) {
+			ObjectLayoutResource.Builder builder =
+				_objectLayoutResourceFactory.create();
+
+			ObjectLayoutResource objectLayoutResource = builder.user(
+				contextUser
+			).build();
+
+			for (ObjectLayout objectLayout : objectLayouts) {
+				objectLayoutResource.postObjectDefinitionObjectLayout(
+					objectDefinitionId, objectLayout);
 			}
 		}
 
