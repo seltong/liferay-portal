dca795c4d550d890d2dc82a35e5c51f3043f3b26
Selton Guedes <selton.guedes@liferay.com>
2023-06-22 17:40:45 -0300

    LPS-184538 Replace localized object field value null with empty string

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -1532,6 +1532,11 @@ public class ObjectEntryLocalServiceImpl
 			values.put(
 				objectFieldColumn.getName() + "i18n",
 				(Serializable)localizedValues);
+
+			values.putIfAbsent(
+				StringUtil.removeLast(
+					objectFieldColumn.getName(), StringPool.UNDERLINE),
+				StringPool.BLANK);
 		}
 	}
 
