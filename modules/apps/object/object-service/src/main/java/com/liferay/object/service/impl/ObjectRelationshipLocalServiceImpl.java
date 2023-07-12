ca26a4778a9a88c5723d203bc41644f45b2b536a
Selton Guedes <selton.guedes@liferay.com>
2023-07-12 15:59:27 -0300

    LPS-172418 Add externalReferenceCode parameter for ObjectRelationshipServiceImpl and ObjectRelationshipLocarlServiceImpl

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
@@ -107,14 +107,16 @@ public class ObjectRelationshipLocalServiceImpl
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public ObjectRelationship addObjectRelationship(
-			long userId, long objectDefinitionId1, long objectDefinitionId2,
-			long parameterObjectFieldId, String deletionType,
-			Map<Locale, String> labelMap, String name, String type)
+			String externalReferenceCode, long userId, long objectDefinitionId1,
+			long objectDefinitionId2, long parameterObjectFieldId,
+			String deletionType, Map<Locale, String> labelMap, String name,
+			String type)
 		throws PortalException {
 
 		return _addObjectRelationship(
-			userId, objectDefinitionId1, objectDefinitionId2,
-			parameterObjectFieldId, deletionType, labelMap, name, false, type);
+			externalReferenceCode, userId, objectDefinitionId1,
+			objectDefinitionId2, parameterObjectFieldId, deletionType, labelMap,
+			name, false, type);
 	}
 
 	@Override
@@ -703,8 +705,9 @@ public class ObjectRelationshipLocalServiceImpl
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public ObjectRelationship updateObjectRelationship(
-			long objectRelationshipId, long parameterObjectFieldId,
-			String deletionType, Map<Locale, String> labelMap)
+			String externalReferenceCode, long objectRelationshipId,
+			long parameterObjectFieldId, String deletionType,
+			Map<Locale, String> labelMap)
 		throws PortalException {
 
 		if (Validator.isNull(deletionType)) {
@@ -733,8 +736,8 @@ public class ObjectRelationshipLocalServiceImpl
 				fetchReverseObjectRelationship(objectRelationship, true);
 
 			_updateObjectRelationship(
-				parameterObjectFieldId, deletionType, labelMap,
-				reverseObjectRelationship);
+				externalReferenceCode, parameterObjectFieldId, deletionType,
+				labelMap, reverseObjectRelationship);
 
 			Indexer<ObjectRelationship> indexer =
 				IndexerRegistryUtil.nullSafeGetIndexer(
@@ -744,7 +747,8 @@ public class ObjectRelationshipLocalServiceImpl
 		}
 
 		objectRelationship = _updateObjectRelationship(
-			parameterObjectFieldId, deletionType, labelMap, objectRelationship);
+			externalReferenceCode, parameterObjectFieldId, deletionType,
+			labelMap, objectRelationship);
 
 		if ((objectRelationship.getObjectFieldId2() != 0) &&
 			StringUtil.equals(
@@ -845,10 +849,10 @@ public class ObjectRelationshipLocalServiceImpl
 	}
 
 	private ObjectRelationship _addObjectRelationship(
-			long userId, long objectDefinitionId1, long objectDefinitionId2,
-			long parameterObjectFieldId, String deletionType,
-			Map<Locale, String> labelMap, String name, boolean reverse,
-			String type)
+			String externalReferenceCode, long userId, long objectDefinitionId1,
+			long objectDefinitionId2, long parameterObjectFieldId,
+			String deletionType, Map<Locale, String> labelMap, String name,
+			boolean reverse, String type)
 		throws PortalException {
 
 		_validate(
@@ -858,6 +862,8 @@ public class ObjectRelationshipLocalServiceImpl
 		ObjectRelationship objectRelationship =
 			objectRelationshipPersistence.create(
 				counterLocalService.increment());
+
+		objectRelationship.setExternalReferenceCode(externalReferenceCode);
 
 		User user = _userLocalService.getUser(userId);
 
@@ -903,9 +909,9 @@ public class ObjectRelationshipLocalServiceImpl
 				objectDefinition1, objectDefinition2, objectRelationship);
 
 			_addObjectRelationship(
-				userId, objectDefinitionId2, objectDefinitionId1,
-				parameterObjectFieldId, deletionType, labelMap, name, true,
-				type);
+				externalReferenceCode, userId, objectDefinitionId2,
+				objectDefinitionId1, parameterObjectFieldId, deletionType,
+				labelMap, name, true, type);
 
 			return objectRelationshipLocalService.
 				createManyToManyObjectRelationshipTable(
@@ -997,9 +1003,11 @@ public class ObjectRelationshipLocalServiceImpl
 	}
 
 	private ObjectRelationship _updateObjectRelationship(
-		long parameterObjectFieldId, String deletionType,
-		Map<Locale, String> labelMap, ObjectRelationship objectRelationship) {
+		String externalReferenceCode, long parameterObjectFieldId,
+		String deletionType, Map<Locale, String> labelMap,
+		ObjectRelationship objectRelationship) {
 
+		objectRelationship.setExternalReferenceCode(externalReferenceCode);
 		objectRelationship.setParameterObjectFieldId(parameterObjectFieldId);
 		objectRelationship.setDeletionType(deletionType);
 		objectRelationship.setLabelMap(labelMap);
