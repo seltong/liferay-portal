d97485a1c3974a39c7861081f2c933e518a220ef
Selton Guedes <selton.guedes@liferay.com>
2023-08-02 10:46:03 -0300

    LPS-172418 Update the ObjectRelationship if not reverse, Create the ObjectRelationship only if it doesn't exists and Update the externalReferenceCode of the reverse ObjectRelationship

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -815,15 +815,44 @@ public class ObjectDefinitionResourceImpl
 				com.liferay.object.model.ObjectRelationship
 					serviceBuilderObjectRelationship =
 						_objectRelationshipLocalService.
-							fetchObjectRelationshipByObjectDefinitionId(
-								objectDefinitionId,
-								objectRelationship.getName());
+							fetchObjectRelationshipByExternalReferenceCode(
+								objectRelationship.getExternalReferenceCode(),
+								contextCompany.getCompanyId(),
+								objectDefinitionId);
 
-				if (serviceBuilderObjectRelationship != null) {
+				if ((serviceBuilderObjectRelationship != null) &&
+					!serviceBuilderObjectRelationship.isReverse()) {
+
 					objectRelationshipResource.putObjectRelationship(
 						serviceBuilderObjectRelationship.
 							getObjectRelationshipId(),
 						objectRelationship);
+
+					continue;
+				}
+
+				serviceBuilderObjectRelationship =
+					_objectRelationshipLocalService.
+						fetchObjectRelationshipByObjectDefinitionId1(
+							objectDefinitionId, objectRelationship.getName());
+
+				if ((serviceBuilderObjectRelationship != null) &&
+					!serviceBuilderObjectRelationship.isReverse() &&
+					!serviceBuilderObjectRelationship.isSelf()) {
+
+					objectRelationshipResource.putObjectRelationship(
+						serviceBuilderObjectRelationship.
+							getObjectRelationshipId(),
+						objectRelationship);
+
+					continue;
+				}
+
+				if (serviceBuilderObjectRelationship != null) {
+					_objectRelationshipLocalService.
+						updateReverseObjectRelationshipExternalReferenceCode(
+							objectRelationship.getExternalReferenceCode(),
+							serviceBuilderObjectRelationship);
 
 					continue;
 				}
