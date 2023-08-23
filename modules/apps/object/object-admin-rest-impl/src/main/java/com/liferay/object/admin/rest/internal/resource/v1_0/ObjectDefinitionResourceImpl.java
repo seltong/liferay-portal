7845348c500329b54d775b7db7b137ab70667cf1
Selton Guedes <selton.guedes@liferay.com>
2023-08-23 18:07:21 -0300

    LPS-193573 Rename

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -53,7 +53,7 @@ import com.liferay.object.service.ObjectViewService;
 import com.liferay.object.system.JaxRsApplicationDescriptor;
 import com.liferay.object.system.SystemObjectDefinitionManager;
 import com.liferay.object.system.SystemObjectDefinitionManagerRegistry;
-import com.liferay.object.system.util.SystemUtil;
+import com.liferay.object.system.util.SystemObjectDefinitionsUtil;
 import com.liferay.object.util.comparator.ObjectFieldCreateDateComparator;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
@@ -540,7 +540,9 @@ public class ObjectDefinitionResourceImpl
 			ListUtil.copy(
 				_objectFieldLocalService.getObjectFields(objectDefinitionId));
 
-		if (SystemUtil.allowManageSystemEntities()) {
+		if (SystemObjectDefinitionsUtil.
+				isAllowedManageSystemObjectDefinitions()) {
+
 			objectFields.removeIf(
 				objectField -> !GetterUtil.getBoolean(objectField.getSystem()));
 
