5696abb724aa64b61ee72f438f8621586c21fa85
Selton Guedes <selton.guedes@liferay.com>
2023-08-31 14:53:26 -0300

    LPS-193573 buildService

--- modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectDefinitionLocalService.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/service/ObjectDefinitionLocalService.java
@@ -295,10 +295,6 @@ public interface ObjectDefinitionLocalService
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public IndexableActionableDynamicQuery getIndexableActionableDynamicQuery();
 
-	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
-	public List<ObjectDefinition> getModifiableObjectDefinitions(
-		long companyId, boolean active, int status);
-
 	/**
 	 * Returns the object definition with the primary key.
 	 *
