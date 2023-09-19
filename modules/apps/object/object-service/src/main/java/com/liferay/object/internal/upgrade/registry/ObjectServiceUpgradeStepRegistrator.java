3be426520e0ca95657f6afd1a9f67e21ba42890c
Selton Guedes <selton.guedes@liferay.com>
2023-09-19 14:01:55 -0300

    LPS-196061 Add upgrade process for system column in ObjectAction table
    Co-authored-by: Guilherme Sa <guilherme.sa@liferay.com>

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -345,6 +345,10 @@ public class ObjectServiceUpgradeStepRegistrator
 			"8.2.0", "8.3.0",
 			new com.liferay.object.internal.upgrade.v8_3_0.
 				ObjectValidationRuleUpgradeProcess());
+
+		registry.register(
+			"8.3.0", "8.4.0",
+			UpgradeProcessFactory.addColumns("ObjectAction", "system BOOLEAN"));
 	}
 
 	@Reference
