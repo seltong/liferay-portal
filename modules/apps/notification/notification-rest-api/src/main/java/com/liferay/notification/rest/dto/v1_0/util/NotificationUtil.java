78ff6dbbb335a4a78d8914709327300e8972a99f
Selton Guedes <selton.guedes@liferay.com>
2023-10-10 09:47:00 -0300

    LPS-198619 Populate system in NotificationTemplate dto

--- modules/apps/notification/notification-rest-api/src/main/java/com/liferay/notification/rest/dto/v1_0/util/NotificationUtil.java
+++ modules/apps/notification/notification-rest-api/src/main/java/com/liferay/notification/rest/dto/v1_0/util/NotificationUtil.java
@@ -172,6 +172,8 @@ public class NotificationUtil {
 		serviceBuilderNotificationTemplate.setSubjectMap(
 			LocalizedMapUtil.getLocalizedMap(
 				notificationTemplate.getSubject()));
+		serviceBuilderNotificationTemplate.setSystem(
+			notificationTemplate.getSystem());
 		serviceBuilderNotificationTemplate.setType(
 			notificationTemplate.getType());
 
