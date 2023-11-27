4b2633cbdd71046f45f626d196c4f426750249de
Selton Guedes <selton.guedes@liferay.com>
2023-11-27 11:14:22 -0300

    WIP

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/action/GetViewListTypeDefinitionsURLMVCResourceCommand.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/portlet/action/GetViewListTypeDefinitionsURLMVCResourceCommand.java
@@ -3,29 +3,30 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.object.web.internal.list.type.portlet.action;
+package com.liferay.object.web.internal.object.definitions.portlet.action;
 
 import com.liferay.object.constants.ObjectPortletKeys;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
-import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
-import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.util.Portal;
 
+import javax.portlet.PortletRequest;
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Selton Guedes
  */
 @Component(
 	property = {
-		"javax.portlet.name=" + ObjectPortletKeys.LIST_TYPE_DEFINITIONS,
-		"mvc.command.name=/list_type_definitions/get_view_list_type_definitions_url"
+		"javax.portlet.name=" + ObjectPortletKeys.OBJECT_DEFINITIONS,
+		"mvc.command.name=/object_definitions/get_view_list_type_definitions_url"
 	},
 	service = MVCResourceCommand.class
 )
@@ -37,19 +38,19 @@ public class GetViewListTypeDefinitionsURLMVCResourceCommand
 			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 		throws Exception {
 
-		RequestBackedPortletURLFactory requestBackedPortletURLFactory =
-			RequestBackedPortletURLFactoryUtil.create(resourceRequest);
-
 		JSONPortletResponseUtil.writeJSON(
 			resourceRequest, resourceResponse,
 			JSONUtil.put(
 				"url",
 				PortletURLBuilder.create(
-					requestBackedPortletURLFactory.createControlPanelRenderURL(
-						ObjectPortletKeys.LIST_TYPE_DEFINITIONS, null, 0, 0)
-				).setMVCRenderCommandName(
-					"/list_type_definitions/view_list_type_definitions"
+					_portal.getControlPanelPortletURL(
+						resourceRequest,
+						ObjectPortletKeys.LIST_TYPE_DEFINITIONS,
+						PortletRequest.RENDER_PHASE)
 				).buildString()));
 	}
+
+	@Reference
+	private Portal _portal;
 
 }
