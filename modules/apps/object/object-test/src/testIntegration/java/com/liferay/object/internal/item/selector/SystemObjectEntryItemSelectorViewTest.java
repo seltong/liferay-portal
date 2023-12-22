dccec4936d95787c85e1d80b1d1bbcde14402de7
Selton Guedes <selton.guedes@liferay.com>
2023-12-22 17:56:59 -0300

    WIP

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/internal/item/selector/SystemObjectEntryItemSelectorViewTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/internal/item/selector/SystemObjectEntryItemSelectorViewTest.java
@@ -7,44 +7,64 @@ package com.liferay.object.internal.item.selector;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.item.selector.ItemSelector;
-import com.liferay.item.selector.ItemSelectorCriterion;
 import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.ItemSelectorViewDescriptor;
 import com.liferay.item.selector.ItemSelectorViewDescriptorRenderer;
 import com.liferay.item.selector.criteria.info.item.criterion.InfoItemItemSelectorCriterion;
 import com.liferay.object.model.ObjectDefinition;
-import com.liferay.object.related.models.ObjectRelatedModelsProviderRegistrarHelper;
 import com.liferay.object.related.models.ObjectRelatedModelsProviderRegistry;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectFieldLocalService;
-import com.liferay.object.service.ObjectFolderLocalService;
 import com.liferay.object.system.SystemObjectDefinitionManager;
 import com.liferay.object.system.SystemObjectDefinitionManagerRegistry;
-import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
-import com.liferay.portal.kernel.concurrent.test.TestUtil;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.model.Release;
-import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.test.rule.Sync;
-import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderRequest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletRenderResponse;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletURL;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.Inject;
-import com.liferay.portal.util.PortalInstances;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.component.annotations.Reference;
+import org.osgi.framework.ServiceReference;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletRequest;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * @author Selton Guedes
@@ -52,83 +72,173 @@ import org.osgi.service.component.annotations.Reference;
 @RunWith(Arquillian.class)
 public class SystemObjectEntryItemSelectorViewTest {
 
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-//		_company = CompanyTestUtil.addCompany(true);
-//
-//		_originalName = PrincipalThreadLocal.getName();
-//
-//		PrincipalThreadLocal.setName(TestPropsValues.getUserId());
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
+	@Before
+	public void setUpClass() throws Exception {
 		Bundle bundle = FrameworkUtil.getBundle(
 			SystemObjectEntryItemSelectorViewTest.class);
 
 		_bundleContext = bundle.getBundleContext();
 
-		_bundleContext.registerService(
-			ItemSelectorView.class,
-			new SystemObjectEntryItemSelectorView(
-				_dtoConverterRegistry, _itemSelector,
-				_itemSelectorViewDescriptorRenderer,
-				_getUserSystemObjectDefinition(), _objectDefinitionLocalService,
-				_objectFieldLocalService, _objectRelatedModelsProviderRegistry,
-				_portal, _systemObjectDefinitionManagerRegistry,
-				_userLocalService),
-			HashMapDictionaryBuilder.<String, Object>put(
-				"item.selector.view.order", 500
-			).build());
+		_serviceReferences =
+			_bundleContext.getServiceReferences(
+				ItemSelectorView.class, "(item.selector.view.order=500)");
 
-	}
-
-	@AfterClass
-	public static void tearDownClass() throws Exception {
-//		_companyLocalService.deleteCompany(_company);
-//
-//		PrincipalThreadLocal.setName(_originalName);
+		_group = GroupTestUtil.addGroup();
 	}
 
 	@Test
-	public void testGetTitle() throws PortalException {
-		_bundleContext.getServiceReference(ItemSelectorView.class);
+	public void testGetTitle() throws Exception {
+		Iterator<ServiceReference<ItemSelectorView>> iterator =
+			_serviceReferences.iterator();
 
-		ObjectDefinition objectDefinition = _getUserSystemObjectDefinition();
+		ServiceReference<ItemSelectorView> serviceReference = iterator.next();
 
-		//		_systemObjectEntryItemSelectorView.getTitle(_company.getLocale());
+		ItemSelectorView itemSelectorView = _bundleContext.getService(serviceReference);
 
-		//		Assert.assertEquals(
-		//			"User ",
-		//			_systemObjectEntryItemSelectorView.getTitle(_company.getLocale()));
+		Queue<ItemSelectorViewDescriptor> itemSelectorViewDescriptors = new LinkedList<>();
+
+		ItemSelectorViewDescriptorRenderer itemSelectorViewDescriptorRenderer =
+			(ItemSelectorViewDescriptorRenderer) ReflectionTestUtil.getAndSetFieldValue(
+				itemSelectorView, "_itemSelectorViewDescriptorRenderer",
+				ProxyUtil.newProxyInstance(
+					ItemSelectorViewDescriptorRenderer.class.getClassLoader(),
+					new Class<?>[] {ItemSelectorViewDescriptorRenderer.class},
+					(proxy, method, arguments) -> {
+						if(StringUtil.equals(method.getName(), "renderHTML")){
+
+							itemSelectorViewDescriptors.add(
+								(ItemSelectorViewDescriptor)arguments[6]);
+						}
+
+						return null;
+					}));
+
+		itemSelectorView.renderHTML(
+			_mockHttpServletRequest(), new MockHttpServletResponse(),
+			new InfoItemItemSelectorCriterion(), new MockLiferayPortletURL(),
+			RandomTestUtil.randomString(), true);
+
+		ItemSelectorViewDescriptor itemSelectorViewDescriptor =
+			itemSelectorViewDescriptors.poll();
+
+		ObjectDefinition objectDefinition = _objectDefinitionLocalService.getObjectDefinitionByExternalReferenceCode(
+			_systemObjectDefinitionsERCs.get(itemSelectorView.getTitle(Locale.getDefault())), _group.getCompanyId());
+
+		SystemObjectDefinitionManager systemObjectDefinitionManager =
+			_systemObjectDefinitionManagerRegistry.
+				getSystemObjectDefinitionManager(
+					objectDefinition.getName());
+
+		ItemSelectorViewDescriptor.ItemDescriptor itemDescriptor =
+			itemSelectorViewDescriptor.getItemDescriptor(
+				systemObjectDefinitionManager.
+			);
+
+		itemDescriptor.getTitle(Locale.getDefault());
+
+		ReflectionTestUtil.setFieldValue(
+			itemSelectorView, "_itemSelectorViewDescriptorRenderer", itemSelectorViewDescriptorRenderer);
 	}
 
-	private static ObjectDefinition _getUserSystemObjectDefinition() {
+	private ThemeDisplay _getThemeDisplay(Group group) throws Exception {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setCompany(
+			CompanyLocalServiceUtil.fetchCompany(group.getCompanyId()));
+		themeDisplay.setLocale(LocaleUtil.getDefault());
+		themeDisplay.setPermissionChecker(
+			PermissionThreadLocal.getPermissionChecker());
+		themeDisplay.setScopeGroupId(group.getGroupId());
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		return themeDisplay;
+	}
+
+	private ServletRequest _mockHttpServletRequest() throws Exception {
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		MockLiferayPortletRenderRequest mockLiferayPortletRenderRequest =
+			new MockLiferayPortletRenderRequest();
+
+		mockLiferayPortletRenderRequest.setAttribute(
+			"null-" + WebKeys.CURRENT_PORTLET_URL, new MockLiferayPortletURL());
+
+		mockHttpServletRequest.setAttribute(
+			JavaConstants.JAVAX_PORTLET_REQUEST,
+			mockLiferayPortletRenderRequest);
+
+		mockHttpServletRequest.setAttribute(
+			JavaConstants.JAVAX_PORTLET_RESPONSE,
+			new MockLiferayPortletRenderResponse());
+
+		ThemeDisplay themeDisplay = _getThemeDisplay(_group);
+
+		themeDisplay.setRequest(mockHttpServletRequest);
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		return mockHttpServletRequest;
+	}
+
+	private ObjectDefinition _getUserSystemObjectDefinition() {
 		return _objectDefinitionLocalService.fetchObjectDefinition(
 			_company.getCompanyId(), "L_USER");
 	}
 
+	private final Map<String, String> _systemObjectDefinitionsERCs =
+		HashMapBuilder.<String, String>put(
+			"Postal Address", "L_POSTAL_ADDRESS"
+	).put(
+			"Account", "L_ACCOUNT"
+		).put(
+			"Organization", "L_ORGANIZATION"
+		).put(
+			"User", "L_USER"
+		).put(
+			"Commerce Product Group", "L_COMMERCE_PRODUCT_GROUP"
+		).put(
+			"Commerce Product", "L_COMMERCE_PRODUCT_DEFINITION"
+		).put(
+			"Commerce Order", "L_COMMERCE_ORDER"
+		).build();
+
 	private static BundleContext _bundleContext;
+	@DeleteAfterTestRun
+	private Group _group;
+	private static Collection<ServiceReference<ItemSelectorView>> _serviceReferences;
 	private static Company _company;
 
 	@Inject
-	private static CompanyLocalService _companyLocalService;
+	private CompanyLocalService _companyLocalService;
 
 	@Inject
-	private static DTOConverterRegistry _dtoConverterRegistry;
+	private DTOConverterRegistry _dtoConverterRegistry;
 
 	@Inject
-	private static ItemSelector _itemSelector;
+	private ItemSelector _itemSelector;
 
 	@Inject
-	private static ItemSelectorViewDescriptorRenderer
+	private ItemSelectorViewDescriptorRenderer
 		<InfoItemItemSelectorCriterion> _itemSelectorViewDescriptorRenderer;
 
 	@Inject
-	private static ObjectDefinitionLocalService _objectDefinitionLocalService;
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
 
 	@Inject
-	private static ObjectFieldLocalService _objectFieldLocalService;
+	private ObjectFieldLocalService _objectFieldLocalService;
 
 	@Inject
-	private static ObjectRelatedModelsProviderRegistry
+	private ObjectRelatedModelsProviderRegistry
 		_objectRelatedModelsProviderRegistry;
 
 	private static String _originalName;
