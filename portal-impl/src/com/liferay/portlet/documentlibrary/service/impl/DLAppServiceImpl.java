077cbfef20b85dd0bc98650ac2ce2d2067f64be8
Selton Guedes <selton.guedes@liferay.com>
2024-02-15 18:17:16 -0300

    Revert "LPD-6960 match the other description for expiration"
    This reverts commit aa680ce19aa598b5a8756325ecf6274cc6134727.

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppServiceImpl.java
@@ -164,7 +164,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @throws     PortalException if a portal exception occurred
 	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
 	 *             #addFileEntry(String, long, long, String, String, String,
-	 *             String, String, File, Date, Date, ServiceContext)}
+	 *             String, String, String, File, Date, Date, Date, ServiceContext)}
 	 */
 	@Deprecated
 	@Override
@@ -204,6 +204,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @param  description the file's description
 	 * @param  changeLog the file's version change log
 	 * @param  bytes the file's data (optionally <code>null</code>)
+	 * @param  displayDate the date when file is set to display (optionally <code>null</code>)
 	 * @param  expirationDate the file's expiration date (optionally <code>null
 	 *                           </code>)
 	 * @param  reviewDate the file's review Date (optionally <code>null</code>)
@@ -268,6 +269,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @param  description the file's description
 	 * @param  changeLog the file's version change log
 	 * @param  file the file's data (optionally <code>null</code>)
+	 * @param  displayDate the date when file is set to display (optionally <code>null</code>)
 	 * @param  expirationDate the file's expiration date (optionally <code>null
 	 *                           </code>)
 	 * @param  reviewDate the file's review Date (optionally <code>null</code>)
@@ -329,6 +331,7 @@ public class DLAppServiceImpl extends DLAppServiceBaseImpl {
 	 * @param  changeLog the file's version change log
 	 * @param  inputStream the file's data (optionally <code>null</code>)
 	 * @param  size the file's size (optionally <code>0</code>)
+	 * @param  displayDate the date when file is set to display (optionally <code>null</code>)
 	 * @param  expirationDate the file's expiration date (optionally <code>null</code>)
 	 * @param  reviewDate the file's review Date (optionally <code>null</code>)
 	 * @param  serviceContext the service context to be applied. Can set the
