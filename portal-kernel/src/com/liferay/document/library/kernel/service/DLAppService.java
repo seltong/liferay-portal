4861037043d0f729fcf592e2fa6358d52b02a93b
Selton Guedes <selton.guedes@liferay.com>
2024-02-15 18:22:45 -0300

    Revert "LPD-6960 buildService"
    This reverts commit 4a08b33a2ce78d6d0dd70ae54a59a5e4014987da.

--- portal-kernel/src/com/liferay/document/library/kernel/service/DLAppService.java
+++ portal-kernel/src/com/liferay/document/library/kernel/service/DLAppService.java
@@ -94,7 +94,7 @@ public interface DLAppService extends BaseService {
 	 * @throws PortalException if a portal exception occurred
 	 * @deprecated As of Cavanaugh (7.4.x), replaced by {@link
 	 #addFileEntry(String, long, long, String, String, String,
-	 String, String, String, File, Date, Date, Date, ServiceContext)}
+	 String, String, File, Date, Date, ServiceContext)}
 	 */
 	@Deprecated
 	public FileEntry addFileEntry(
@@ -127,7 +127,6 @@ public interface DLAppService extends BaseService {
 	 * @param description the file's description
 	 * @param changeLog the file's version change log
 	 * @param bytes the file's data (optionally <code>null</code>)
-	 * @param displayDate the date when file is set to display (optionally <code>null</code>)
 	 * @param expirationDate the file's expiration date (optionally <code>null
 	 </code>)
 	 * @param reviewDate the file's review Date (optionally <code>null</code>)
@@ -170,7 +169,6 @@ public interface DLAppService extends BaseService {
 	 * @param description the file's description
 	 * @param changeLog the file's version change log
 	 * @param file the file's data (optionally <code>null</code>)
-	 * @param displayDate the date when file is set to display (optionally <code>null</code>)
 	 * @param expirationDate the file's expiration date (optionally <code>null
 	 </code>)
 	 * @param reviewDate the file's review Date (optionally <code>null</code>)
@@ -214,7 +212,6 @@ public interface DLAppService extends BaseService {
 	 * @param changeLog the file's version change log
 	 * @param inputStream the file's data (optionally <code>null</code>)
 	 * @param size the file's size (optionally <code>0</code>)
-	 * @param displayDate the date when file is set to display (optionally <code>null</code>)
 	 * @param expirationDate the file's expiration date (optionally <code>null</code>)
 	 * @param reviewDate the file's review Date (optionally <code>null</code>)
 	 * @param serviceContext the service context to be applied. Can set the
