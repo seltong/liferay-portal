ec4eaed27deeacae5bca2cd341aa0ed02836f6ff
Selton Guedes <selton.guedes@liferay.com>
2024-02-22 15:56:17 -0300

    LPD-16334 Use KaleoDefinitionVersionService instead of KaleoDefinitionVersionLocalService

--- modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowDefinitionManagerImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-runtime-integration-impl/src/main/java/com/liferay/portal/workflow/kaleo/runtime/integration/internal/WorkflowDefinitionManagerImpl.java
@@ -31,6 +31,7 @@ import com.liferay.portal.workflow.kaleo.runtime.util.comparator.KaleoDefinition
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionLocalService;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionService;
 import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
+import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionService;
 import com.liferay.portal.workflow.manager.WorkflowDefinitionManager;
 
 import java.util.ArrayList;
@@ -276,7 +277,7 @@ public class WorkflowDefinitionManagerImpl
 
 		try {
 			return _kaleoWorkflowModelConverter.toWorkflowDefinition(
-				_kaleoDefinitionVersionLocalService.getKaleoDefinitionVersion(
+				_kaleoDefinitionVersionService.getKaleoDefinitionVersion(
 					companyId, name, getVersion(version)));
 		}
 		catch (NoSuchModelException noSuchModelException) {
@@ -501,6 +502,8 @@ public class WorkflowDefinitionManagerImpl
 	@Reference
 	private KaleoDefinitionVersionLocalService
 		_kaleoDefinitionVersionLocalService;
+
+	private KaleoDefinitionVersionService _kaleoDefinitionVersionService;
 
 	@Reference
 	private KaleoWorkflowModelConverter _kaleoWorkflowModelConverter;
--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoDefinitionVersionServiceImpl.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/service/impl/KaleoDefinitionVersionServiceImpl.java
@@ -6,9 +6,15 @@
 package com.liferay.portal.workflow.kaleo.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion;
+import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalService;
 import com.liferay.portal.workflow.kaleo.service.base.KaleoDefinitionVersionServiceBaseImpl;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -22,4 +28,30 @@ import org.osgi.service.component.annotations.Component;
 )
 public class KaleoDefinitionVersionServiceImpl
 	extends KaleoDefinitionVersionServiceBaseImpl {
+
+	public KaleoDefinitionVersion getKaleoDefinitionVersion(
+			long companyId, String name, String version)
+		throws PortalException {
+
+		KaleoDefinitionVersion kaleoDefinitionVersion =
+			_kaleoDefinitionVersionLocalService.getKaleoDefinitionVersion(
+				companyId, name, version);
+
+		_kaleoDefinitionVersionModelResourcePermission.check(
+			getPermissionChecker(),
+			kaleoDefinitionVersion.getKaleoDefinitionVersionId(),
+			ActionKeys.VIEW);
+
+		return kaleoDefinitionVersion;
+	}
+
+	private KaleoDefinitionVersionLocalService
+		_kaleoDefinitionVersionLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.portal.workflow.kaleo.model.KaleoDefinitionVersion)"
+	)
+	private ModelResourcePermission<KaleoDefinitionVersion>
+		_kaleoDefinitionVersionModelResourcePermission;
+
 }
